9f96fd62ae215b1aaebf70505ab9f9077fe48bc5
Leehwa531 <qwg531@naver.com>
2024-11-21 15:27:21 +0900

    learn:24년11월21일

--- 14889_스타트와-링크.py
+++ 14889_스타트와-링크.py
@@ -1,27 +1,29 @@
-def cal(alst,blst):
-  asm = bsm = 0
-  for i in range(M):
-    for j in range(M):
-      asm += arr[alst[i][alst[j]]]
-      bsm += arr[blst[i][blst[j]]]
-  return abs(asm-bsm)
+def cal(alst, blst):
+    asm = bsm = 0
+    for i in range(M):
+        for j in range(M):
+            asm += arr[alst[i]][alst[j]]
+            bsm += arr[blst[i]][blst[j]]
+    return abs(asm - bsm)
 
-def dfs(n,alst,blst):
-  global ans
-  if n==N:
-    if len(alst)==len(blst):
-      ans = min(ans, cal(alst,blst))
-    return
-  
-  dfs(n+1,alst+[n],blst) # A팀 선택
-  dfs(n+1,alst, blst+[n]) # B팀 선택
+def dfs(n, alst, blst):
+    global ans
+    if n == N:
+        if len(alst) == len(blst):  # A팀과 B팀의 크기가 같을 경우에만
+            ans = min(ans, cal(alst, blst))  # 능력치 차이의 최솟값 갱신
+        return
+    
+    # A팀에 추가
+    dfs(n + 1, alst + [n], blst)  
+    # B팀에 추가
+    dfs(n + 1, alst, blst + [n])  
 
-N = int(input())
+N = int(input())  # 사람 수
+arr = [list(map(int, input().split())) for _ in range(N)]  # 능력치 배열
+M = N // 2  # 한 팀의 인원 수
 
-arr = [(list(map(int,input().split()))) for _ in range(N)]
+ans = 100 * M * M  # 최솟값 갱신을 위한 초기값
 
-M = N//2
+dfs(0, [], [])  # 백트래킹 시작
 
-ans = 100*M*M
-dfs(0,[],[])
-print(ans)
+print(ans)  # 결과 출력
---
+++ 3046_R2.py
@@ -0,0 +1,3 @@
+R1, R2 = map(int,input().split())
+R3= (R2 *2) - R1
+print(R3)
